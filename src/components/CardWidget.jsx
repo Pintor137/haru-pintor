5b4721d8335bff84c7ead39a5a26529fbe03bae5
javier <Pintor137>
2021-10-14 23:30:55 -0500

    Ajustes

---
+++ src/components/CardWidget.jsx
@@ -0,0 +1,13 @@
+
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { IconButton} from '@material-ui/core'
+export const CardWidget = () => {
+    return (
+        <>
+            <IconButton color="inherit" arial-label="menu">
+                <ShoppingCartIcon />
+                0
+            </IconButton>
+        </>
+    )
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Box,Grid} from '@material-ui/core'
+
+export const Product = () => {
+    return (
+        <>
+
+            <Grid container mt={10}>
+                <Grid item xs={12}>
+                    Hola
+                </Grid>
+
+                <Grid item xs={12}>
+                    Hola
+                </Grid>
+
+                <Grid item xs={12}>
+                    Hola
+                </Grid>
+
+                <Grid item xs={12}>
+                    Hola
+                </Grid>
+
+            </Grid>
+            
+        </>
+    )
+}
